31742a509aef83819574619f7ba84b3d46af9c7d
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-01-09 23:14:29 -0300

    Primeiras aulas

---
+++ Aula 11/Ex08.js
@@ -0,0 +1,7 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro e ${vel}Km/h`)
+
+if(vel > 60){ // condicao simples
+    console.log(`Voce utrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
---
+++ Aula 11/Ex09.js
@@ -0,0 +1,7 @@
+let pais = "EUA"
+console.log(`Vivendo em ${pais}`)
+if (pais == "Brasil"){
+    console.log("Brasileiro!")
+} else {
+    console.log("Estrangeiro!")
+}
