cebf8cc4651ecf9b6e4dd0a0e3b4d49d6184290e
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-01-11 18:16:50 -0300

    Finalizando ultimo exercicio modulo D

--- Aula 12 Exercicios/Ex14/script.js
+++ Aula 12 Exercicios/Ex14/script.js
@@ -1,1 +1,21 @@
-alert("Ola")
+function carregar(){
+    let msg = window.document.getElementById('msg');
+    let img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var min = data.getMinutes();
+
+    msg.innerHTML = `Agora sao ${hora} horas e ${min} minutos.`
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
+
+
