5fafd3affe31e49ba87fc9b1b80bb87c11ffce48
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-01-13 11:20:04 -0300

    Finalizando Exercicio

--- Aula 14 Exercicios/Ex16/script.js
+++ Aula 14 Exercicios/Ex16/script.js
@@ -1,1 +1,28 @@
-alert("Ola")
+function contador(){
+    var ini = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var passo = window.document.getElementById('passo')
+    var res = window.document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = `Impossivel contar`
+        window.alert("[ERRO]Verifique os dados e tente novamente")
+    } else {
+        res.innerHTML = `Contando: `
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            //contagem crescente
+            for(let c = i; c<=f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            //contagem decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
